64396a944783d6af00f968ab7d3779f982bfbac4
Muyi2905 <obaremimuyiwa@gmail.com>
2024-09-24 12:02:13 +0100

    User creation in db

---
+++ controllers/user_controller.go
@@ -0,0 +1,36 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/muyi2905/models"
+	"gorm.io/gorm"
+)
+
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "encoding/json")
+	var user []models.User
+
+	if details := db.Find(&user); details.Error != nil {
+		http.Error(w, details.Error.Error(), http.StatusInternalServerError)
+	}
+
+	if err := json.NewEncoder(w).Encode(&user); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+}
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "encoding/json")
+
+	var user models.User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		http.Error(w, "invalid request", http.StatusBadRequest)
+	}
+
+	if details := db.Create(&user); details.Error != nil {
+		http.Error(w, details.Error.Error(), http.StatusInternalServerError)
+	}
+}
