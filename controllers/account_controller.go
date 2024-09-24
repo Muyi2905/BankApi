4a9bf7e20ae7ca925065668a6b0dc563ba65d1b0
Muyi2905 <obaremimuyiwa@gmail.com>
2024-09-24 09:34:09 +0100

    logic get account

--- controllers/account_controller.go
+++ controllers/account_controller.go
@@ -17,5 +17,22 @@ func CreateAccount(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(w, "invalid request", http.StatusBadRequest)
 	}
+	result := db.Create(&account)
+	if result.Error != nil {
+		http.Error(w, "error creating account", http.StatusInternalServerError)
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(account)
+}
+
+func GetAccounts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "encoding/json")
+	var accounts []models.Account
+	results := db.Find(&accounts); results.Error!= nil{
+		http.Error(w, results.Error.Error(), http.StatusInternalServerError)
+		return 
+	}
+	json.NewEncoder(w).Encode(&accounts)
 
 }
